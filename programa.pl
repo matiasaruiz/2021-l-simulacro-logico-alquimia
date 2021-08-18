e43ff10537a0d5a1137a1cd837faf0f79663c9ed
Matias Ruiz <39162132+matiasaruiz@users.noreply.github.com>
2021-08-18 18:12:06 -0300

    terminado despues de ver la clase y corregido

--- programa.pl
+++ programa.pl
@@ -7,50 +7,53 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % 1. Modelar los jugadores y elementos y agregarlos a la base de conocimiento, utilizando los ejemplos provistos.
-jugador(ana).
-jugador(beto).
-jugador(cata).
+
+jugador(Jugador) :- 
+    tiene(Jugador, _).
+ 
+elementos(ana, [agua, vapor, tierra, hierro]).
+elementos(beto, Elementos) :- 
+    elementos(ana, Elementos).
+elementos(cata, [fuego, tierra, agua, aire]).
 
 % tiene(Jugador, Elemento)
-elementosDe(ana, [agua, agua, tierra, hierro]).
-elementosDe(beto, [agua, agua, tierra, hierro]).
-elementosDe(cata, [fuego, tierra, agua, aire]).
 
 tiene(Persona, Elemento) :-
-    elementosDe(Persona, ElementosQueTiene),
+    elementos(Persona, ElementosQueTiene),
     member(Elemento, ElementosQueTiene).
 
-% receta(Elemento, [Ingredientes])
-receta(pasto, [agua, tierra]).
-receta(hierro, [fuego, agua, tierra]).
-receta(huesos, [pasto, agua]).
-receta(vapor, [agua, fuego]).
-receta(presion, [hierro, vapor]).
-receta(silicio, [tierra]).
-receta(plastico, [hierro, vapor]).
-receta(playStation, [silicio, hierro, plastico]).
+requiere(pasto, [agua, tierra]).
+requiere(hierro, [fuego, agua, tierra]).
+requiere(huesos, [pasto, agua]).
+requiere(presion, [hierro, vapor]).
+requiere(vapor, [agua, fuego]).
+requiere(playstation, [silicio, hierro, plastico]).
+requiere(silicio, [tierra]).
+requiere(plastico, [huesos, presion]).
+
+requiereElemento(ElementoCompuesto, Material) :-
+    requiere(ElementoCompuesto, Materiales),
+    member(Material, Materiales).
+  
+cantidadDeIngredientes(ElementoCompuesto, Cantidad) :-
+    requiere(ElementoCompuesto, Materiales),
+    length(Materiales, Cantidad).
+  
+elemento(Elemento) :- 
+    tiene(_, Elemento).
+elemento(Elemento) :- 
+    requiereElemento(Elemento, _).
+elemento(Elemento) :- 
+    requiereElemento(_, Elemento).
+
 
 % 2. Saber si un jugador tieneIngredientesPara construir un elemento, 
 %  que es cuando tiene ahora en su inventario todo lo que hace falta.
 %  Por ejemplo, ana tiene los ingredientes para el pasto, pero no para el vapor. 
-
-tieneIngredientesPara(Jugador, Receta):-
+tieneIngredientesPara(Jugador, ElementoCompuesto) :-
     jugador(Jugador),
-    receta(Receta, _),
-    forall(requiereElemento(Receta, Material), tiene(Jugador, Material)).
-
-requiereElemento(Receta, Material) :-
-    receta(Receta, Materiales),
-    member(Material, Materiales).
-  
-:- begin_tests(tieneIngredientesPara).
-    test(tiene_ingredientes_para_la_receta):-
-        tieneIngredientesPara(ana, pasto).
-    test(no_tiene_ingredientes_para_la_receta, fail):-
-        tieneIngredientesPara(ana, vapor).
-    test(que_puede_hacer, set(Que=[pasto,silicio])):-
-        tieneIngredientesPara(ana, Que).
-:- end_tests(tieneIngredientesPara).
+    requiere(ElementoCompuesto, _),
+    forall(requiereElemento(ElementoCompuesto, Material), tiene(Jugador, Material)).
 
 % 3. Saber si un elemento estaVivo. Se sabe que el agua, el fuego 
 % y todo lo que fue construido a partir de alguno de ellos, están vivos. 
@@ -74,21 +77,88 @@ estaVivo(Elemento):-
         estaVivo(Quien).
 :- end_tests(estaVivo).
 
+% 4. Conocer las personas que puedeConstruir un elemento, para lo que se necesita tener los ingredientes 
+% ahora en el inventario y además contar con una o más herramientas que sirvan para construirlo. 
 
+% Para los elementos vivos sirve el libro de la vida
+% para los elementos no vivos el libro inerte). 
+% las cucharas y círculos sirven cuando soportan la cantidad de ingredientes del elemento 
+%       - las cucharas soportan tantos ingredientes como centímetros de longitud/10, 
+%       - los círculos alquímicos soportan tantos ingredientes como metros de diámetro * cantidad de niveles
+% Por ejemplo, beto puede construir el silicio (porque tiene tierra y tiene el libro inerte, 
+% que le sirve para el silicio), 
+% pero no puede construir la presión (porque a pesar de tener hierro y vapor, 
+% no cuenta con herramientas que le sirvan para la presión). 
+% Ana, por otro lado, sí puede construir silicio y presión.
 
+% puedeConstruir(Elemento, Persona)
+puedeConstruir(Persona, Elemento) :-
+    tieneIngredientesPara(Persona, Elemento),
+    tieneHerramientaParaConstruir(Persona, Elemento).
+  
+tieneHerramientaParaConstruir(Persona, Elemento) :-
+    herramienta(Persona, Herramienta),
+    sirveParaConstruir(Herramienta, Elemento).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%% Base de Conocimientos %%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+sirveParaConstruir(libro(vida), Elemento) :- 
+    estaVivo(Elemento).
+sirveParaConstruir(libro(inerte), Elemento) :- 
+    not(estaVivo(Elemento)).
 
+sirveParaConstruir(Herramienta, Elemento) :- 
+    cantidadSoportada(Herramienta, CantidadSoportada),
+    cantidadDeIngredientes(Elemento, CantidadIngredientes),
+    CantidadSoportada >= CantidadIngredientes.
 
-herramienta(ana, circulo(50,3)).
-herramienta(ana, cuchara(40)).
-herramienta(beto, circulo(20,1)).
-herramienta(beto, libro(inerte)).
-herramienta(cata, libro(vida)).
-herramienta(cata, circulo(100,5)).
+% cantidadSoportada(Herramienta, Cantidad)
+cantidadSoportada(cuchara(Longitud), Cantidad) :-
+    Cantidad is Longitud / 10.
+cantidadSoportada(circulo(Diametro, Niveles), Cantidad) :-
+    Cantidad is Diametro / 100 * Niveles.
+  
 
-% Los círculos alquímicos tienen diámetro en cms y cantidad de niveles.
-% Las cucharas tienen una longitud en cms.
-% Hay distintos tipos de libro.
+% 5. Saber si alguien es todopoderoso, que es cuando tiene todos los elementos primitivos 
+% (los que no pueden construirse a partir de nada) y además cuenta con herramientas 
+% que sirven para construir cada elemento que no tenga.
+% Por ejemplo, cata es todopoderosa, pero beto no. 
+
+todopoderoso(Persona) :-
+    jugador(Persona),
+    tieneTodosLosPrimitivos(Persona),
+    tieneHerramientasParaFaltantes(Persona).
+  
+tieneTodosLosPrimitivos(Persona) :-
+    forall(primitivo(Elemento), tiene(Persona, Elemento)).
+
+primitivo(Elemento) :- 
+    elemento(Elemento),
+    not(requiereElemento(Elemento, _)).
+
+tieneHerramientasParaFaltantes(Persona) :-
+    forall(leFalta(Persona, Elemento), tieneHerramientaParaConstruir(Persona, Elemento)).
+
+leFalta(Persona, Elemento) :-
+    elemento(Elemento),
+    not(tiene(Persona, Elemento)).
+  
+% 6. Conocer quienGana, que es quien puede construir más cosas.
+% Por ejemplo, cata gana, pero beto no.
+
+quienGana(Persona) :-
+    jugador(Persona),
+    forall(contrincante(Persona, Contrincante), contruyeMasCosas(Persona, Contrincante)).
+  
+contrincante(Persona, Contrincante) :-
+    jugador(Persona), jugador(Contrincante),
+    Persona \= Contrincante.
+
+contruyeMasCosas(Ganador, Perdedor) :-
+    cantidadDeElementosQuePuedeConstruir(Ganador, Mayor),
+    cantidadDeElementosQuePuedeConstruir(Perdedor, Menor),
+    Mayor > Menor.
+
+cantidadDeElementosQuePuedeConstruir(Persona, Cantidad) :-
+    findall(Elemento, puedeConstruir(Persona, Elemento), Elementos),
+    list_to_set(Elementos, ElementosSinRepetidos),
+    length(ElementosSinRepetidos, Cantidad).
+  
