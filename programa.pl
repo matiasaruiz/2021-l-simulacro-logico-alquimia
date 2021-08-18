a42ea57d2127d7e4333d7ea34aa46aea2855a953
Matias Ruiz <39162132+matiasaruiz@users.noreply.github.com>
2021-08-17 22:48:28 -0300

    first commit

---
+++ programa.pl
@@ -0,0 +1,17 @@
+% Ana tiene agua, vapor, tierra y hierro. 
+% Beto tiene lo mismo que Ana. 
+% Cata tiene fuego, tierra, agua y aire, pero no tiene vapor.
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%% Base de Conocimientos %%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+herramienta(ana, circulo(50,3)).
+herramienta(ana, cuchara(40)).
+herramienta(beto, circulo(20,1)).
+herramienta(beto, libro(inerte)).
+herramienta(cata, libro(vida)).
+herramienta(cata, circulo(100,5)).
